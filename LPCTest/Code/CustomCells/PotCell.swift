e912c168c936a3f74c54da78eecb863026934ccf
Aymen Bokri <aymen.bokri16@gmail.com>
2017-08-09 16:36:24 +0200

    Configure cell and use Kingfisher to load and cache images

--- LPCTest/Code/CustomCells/PotCell.swift
+++ LPCTest/Code/CustomCells/PotCell.swift
@@ -8,16 +8,28 @@
 
 import UIKit
 import Reusable
+import Kingfisher
 
 class PotCell : UITableViewCell, NibReusable {
+    
+    // MARK: - Properties
     
     @IBOutlet internal weak var titleLabel: UILabel!
     @IBOutlet internal weak var typeImageView: UIImageView!
     
     
-    //TODO
     
-    func configureCellFor( pot: Pot){
-        //TODO
+    func configureCellFor(pot: Pot){
+        
+        self.titleLabel.text = pot.name
+        
+        if let path = pot.imageUrl {
+            
+            let url = URL(string: path)
+            
+            self.typeImageView.kf.setImage(with: url, placeholder: Asset.logo.image, options: nil, progressBlock: nil, completionHandler: nil)
+        
+        }
+        
     }
 }
