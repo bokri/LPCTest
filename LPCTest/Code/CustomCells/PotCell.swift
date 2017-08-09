cec5ab4827a890072a34354b71fbe60bda762eeb
Aymen Bokri <aymen.bokri16@gmail.com>
2017-08-09 15:24:00 +0200

    Create a custom pot cell

--- LPCTest/Code/CustomCells/PotCell.swift
+++ LPCTest/Code/CustomCells/PotCell.swift
@@ -1,13 +1,22 @@
+//
+//  PotCell.swift
+//  LPCTest
+//
+//  Created by Aymen Bokri on 09/08/2017.
+//  Copyright © 2017 Saif Chaouachi. All rights reserved.
+//
+
 import UIKit
 
 class PotCell : UITableViewCell {
-
-   @IBOutlet internal weak var titleLabel: UILabel!
-   @IBOutlet internal weak var typeImageView: UIImageView!
-
- //TODO
-
-  func configureCellFor( pot: Pot){
+    
+    @IBOutlet internal weak var titleLabel: UILabel!
+    @IBOutlet internal weak var typeImageView: UIImageView!
+    
+    
     //TODO
-  }
+    
+    func configureCellFor( pot: Pot){
+        //TODO
+    }
 }
