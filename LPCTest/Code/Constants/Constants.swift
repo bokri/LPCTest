8aa8948596ba1dff84c9cdacf14d095c8f000441
Aymen Bokri <aymen.bokri16@gmail.com>
2017-08-10 11:38:00 +0200

    Specify web service constants

---
+++ LPCTest/Code/Constants/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  LPCTest
+//
+//  Created by Aymen Bokri on 10/08/2017.
+//  Copyright © 2017 Saif Chaouachi. All rights reserved.
+//
+
+import Foundation
+
+final class Constants {
+    
+    public static let potsBaseUrl = "https://recrutement.lepotcommuntest.fr/recruting"
+    
+    public static let getPotsSuffix = "/get-pots"
+    
+    public static let createPotSuffix = "/create-pot"
+    
+    public static let deletePotSuffix = "/remove-pot"
+    
+}
